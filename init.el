237b2811ecb54a6e0a6778346286d35ffc463c52
Kostya Dubinin <kostya.dubinin@gmail.com>
2017-03-15 10:07:32 +0100

    Update init.el

--- init.el
+++ init.el
@@ -1,16 +1,27 @@
-;; packages to install:
-;;   undo-tree
-;;   flymake-ruby
-;;   flymake-haml
-;;   magit
-;;   go-mode
-;;
+;; Run package-install-selected-packages
+;; TODO: Add new line at the end of the file.
+
 (require 'package)
 
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-mode typescript-mode yaml-mode flymake-haml haml-mode undo-tree ace-window avy rubocop flymake-ruby magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (setq-default fill-column 80)
 
